e9c78202f38ec86fc17ce8f64b71eed2fdffe309
rokoc003 <roxane.kocem@yahoo.de>
2020-03-11 15:06:01 +0100

    Remove unnecessary div tags

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
