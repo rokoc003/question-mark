37a4fb84b8552c423d31e7a155d4acd069af8d93
Roxane Kocem <54837109+rokoc003@users.noreply.github.com>
2020-03-11 16:46:50 +0100

    Implement react router (#6)

---
+++ src/components/pages/Result.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Result() {
+  return (
+    <div>
+      <Link to="/">Add</Link>
+    </div>
+  );
+}
+export default Result;
