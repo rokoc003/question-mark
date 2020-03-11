956ad501aa0361e33866e5adf0767a188f2ac833
rokoc003 <roxane.kocem@yahoo.de>
2020-03-11 16:40:27 +0100

    Implement react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Add from "./components/pages/Add";
+import Vote from "./components/pages/Vote";
+import Result from "./components/pages/Result";
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Add />
+          </Route>
+          <Route path="/Vote">
+            <Vote />
+          </Route>
+          <Route path="/Result">
+            <Result />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
