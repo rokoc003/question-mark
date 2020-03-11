67a0eb11f6582e8e1cac1584bb9cb34d899ff8a8
Roxane Kocem <54837109+rokoc003@users.noreply.github.com>
2020-03-11 15:07:22 +0100

    Page layout (#5)
    * Implement font and change body
    
    * Style header and add logo
    
    * Remove group tags
    
    * Remove unnecessary div tags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Question-Mark</div>;
+  return <Header />;
 }
 
 export default App;
