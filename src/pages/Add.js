240aaddccee02853237b3c065c1cbd7be2209b20
rokoc003 <roxane.kocem@yahoo.de>
2020-03-12 12:03:17 +0100

    Add functionality to form

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -4,33 +4,71 @@ import "./Add.css";
 import Button from "../components/Button";
 
 function Add() {
+  const [question, setQuestion] = React.useState("");
+  const [answerOne, setAnswerOne] = React.useState("");
+  const [answerTwo, setAnswerTwo] = React.useState("");
+  const [answerThree, setAnswerThree] = React.useState("");
+  const [answerFour, setAnswerFour] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const poll = {
+      question: question,
+      answerOne: answerOne,
+      answerTwo: answerTwo,
+      answerThree: answerThree,
+      answerFour: answerFour
+    };
+    alert(JSON.stringify(poll));
+  }
   return (
     <Card>
-      <form className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <input
           class="add-form__input add-form__input-question"
           type="text"
           placeholder="Enter your question"
+          value={question}
+          onChange={event => {
+            setQuestion(event.target.value);
+          }}
         />
         <input
           class="add-form__input add-form__input-answer"
           type="text"
           placeholder="• First Answer"
+          value={answerOne}
+          onChange={event => {
+            setAnswerOne(event.target.value);
+          }}
         />
         <input
           class="add-form__input add-form__input-answer"
           type="text"
           placeholder="• Second Answer"
+          value={answerTwo}
+          onChange={event => {
+            setAnswerTwo(event.target.value);
+          }}
         />
         <input
           class="add-form__input add-form__input-answer"
           type="text"
           placeholder="• Third Answer"
+          value={answerThree}
+          onChange={event => {
+            setAnswerThree(event.target.value);
+          }}
         />
         <input
           class="add-form__input add-form__input-answer"
           type="text"
           placeholder="• Fourth Answer"
+          value={answerFour}
+          onChange={event => {
+            setAnswerFour(event.target.value);
+          }}
         />
         <Button>Create Poll</Button>
       </form>
